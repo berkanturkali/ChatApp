54b62dd24421bec66ec1a7e2601fd6b0abcd7b0e
berkanturkali <brktrkali@outlook.com>
2021-06-02 02:35:17 +0300

    add nav_graph.xml

---
+++ app/src/main/java/com/example/chatapp/view/authflow/FragmentLogin.kt
@@ -0,0 +1,13 @@
+package com.example.chatapp.view.authflow
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.chatapp.R
+
+class FragmentLogin: Fragment(R.layout.fragment_login_layout) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/chatapp/view/authflow/FragmentSignup.kt
@@ -0,0 +1,13 @@
+package com.example.chatapp.view.authflow
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.chatapp.R
+
+class FragmentSignup : Fragment(R.layout.fragment_signup_layout) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
