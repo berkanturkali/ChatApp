82e51020c67c9a99f8e5dfac3d98d35e65a5f8ef
berkanturkali <brktrkali@outlook.com>
2021-06-02 23:33:24 +0300

    add joinRoom

--- app/src/main/java/com/example/chatapp/view/homeflow/FragmentChats.kt
+++ app/src/main/java/com/example/chatapp/view/homeflow/FragmentChats.kt
@@ -1,6 +1,7 @@
 package com.example.chatapp.view.homeflow
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -12,6 +13,9 @@ import com.example.chatapp.utils.handleResource
 import com.example.chatapp.utils.showSnack
 import com.example.chatapp.viewmodel.homeflow.FragmentChatsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import io.socket.client.Socket
+
+import javax.inject.Inject
 
 private const val TAG = "FragmentChats"
 
@@ -24,14 +28,17 @@ class FragmentChats : Fragment(R.layout.fragment_chats_layout) {
     private val mViewModel: FragmentChatsViewModel by viewModels()
     private lateinit var mAdapter: RoomAdapter
 
+    @Inject
+    lateinit var socket: Socket
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentChatsLayoutBinding.bind(view)
         mViewModel.getRooms()
         subscribeObservers()
-
-
+        listenMessages()
+        initButtons()
     }
 
     private fun initRecycler() {
@@ -44,6 +51,13 @@ class FragmentChats : Fragment(R.layout.fragment_chats_layout) {
         }
     }
 
+    private fun initButtons() {
+        binding.sendMsgBtn.setOnClickListener {
+            socket.emit("messageToServer", binding.messageEt.text.toString().trim())
+            binding.messageEt.setText("")
+        }
+    }
+
     private fun subscribeObservers() {
         mViewModel.rooms.observe(viewLifecycleOwner) {
             handleResource(
@@ -51,12 +65,23 @@ class FragmentChats : Fragment(R.layout.fragment_chats_layout) {
                 successFunc = {
                     initRecycler()
                     mAdapter.submitList(it.data!!)
+                    joinRoom(it.data[0].name)
                 },
                 errorFunc = {
                     binding.root.showSnack(R.color.colorDanger, it.message!!)
                 }
             )
         }
+    }
+
+    private fun listenMessages() {
+        socket.on("messageToClient") {
+            Log.i(TAG, "listenMessages: ${it[0]}")
+        }
+    }
+
+    private fun joinRoom(room: String) {
+        socket.emit("roomToJoin", room)
     }
 
     override fun onDestroyView() {
